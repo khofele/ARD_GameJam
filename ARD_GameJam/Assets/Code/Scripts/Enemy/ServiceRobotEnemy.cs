80a64c5c63152f9b7e73f2663b1974ff4a7a2845
khofele <karen.hofele@gmx.de>
2026-08-22 16:10:27 +0200

    [edited] enemy child classes: removed old method

--- ARD_GameJam/Assets/Code/Scripts/Enemy/ServiceRobotEnemy.cs
+++ ARD_GameJam/Assets/Code/Scripts/Enemy/ServiceRobotEnemy.cs
@@ -3,9 +3,4 @@ using UnityEngine;
 public class ServiceRobotEnemy : Enemy
 {
     // TODO implement
-
-    protected override void UpdateMovement()
-    {
-        // TODO might not be needed --> if no, remove in parent class
-    }
 }
