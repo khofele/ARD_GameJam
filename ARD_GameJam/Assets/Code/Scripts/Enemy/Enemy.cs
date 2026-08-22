ef1f638025e6581ae11796b5ea369ae9fc9764a9
khofele <karen.hofele@gmx.de>
2026-08-22 14:45:53 +0200

    [added] enemy parent class setup + enemy folder

---
+++ ARD_GameJam/Assets/Code/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    protected float m_maxHealth = 100.0f;
+    protected float m_currentHealth = 0.0f;
+    protected float m_lidThreshold = 0.2f;
+    protected bool m_isWeakeningNeeded = false;
+    protected bool m_isLidOpenable = false;
+    protected bool m_isLidOpen = false;
+
+    protected void Start()
+    {
+        m_currentHealth = m_maxHealth;
+    }
+
+    protected void OnTriggerEnter(Collider other) // TODO child classes/prefabs need trigger!!
+    {
+        if(other.gameObject.GetComponent<CharController>() != null)
+        {
+            if (m_isWeakeningNeeded == true)
+            {
+                if (m_currentHealth <= m_maxHealth * m_lidThreshold)
+                {
+                    m_isLidOpenable = true; // TODO Lid can be opened --> display Button, player needs to press E or so --> set isLidOpen = true --> Starts Minigame, stops enemy ai
+                    // TODO maybe: add property for corresponding char state after hacking
+                }
+            }
+            else
+            {
+                m_isLidOpenable = true;
+            }
+        }
+    }
+
+    protected void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharController>() != null)
+        {
+            m_isLidOpenable = false;
+        }
+    }
+
+    protected abstract void UpdateMovement();
+}
