b477656286f21fd895cb5a39fa5ef46a55ed6a92
khofele <karen.hofele@gmx.de>
2026-08-22 02:12:47 +0200

    [added] character controller

---
+++ ARD_GameJam/Assets/Code/Scripts/Character/CharController.cs
@@ -0,0 +1,117 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharController : MonoBehaviour
+{
+    // CONST FIELDS
+    private const string m_defaultHumanInputName = "Default_Human";
+    private const string m_boxRobotInputName = "Box_Robot";
+    private const string m_hammerRobotInputName = "Hammer_Robot";
+    private const string m_serviceRobotInputName = "Service_Robot";
+    private const float m_maxHealth = 100.0f;
+
+    // PRIVATE FIELDS
+    private CharStates m_currentCharState = CharStates.DEFAULT_HUMAN;
+    private CharMovement m_currentMovement = null;
+    private float m_currentHealth = 0.0f;
+
+    // SERIALIZE FIELDS
+    [SerializeField] private PlayerInput m_playerInput = null;
+    [SerializeField] private HumanMovement m_humanMovement = null;
+    [SerializeField] private BoxRobotMovement m_boxRobotMovement = null;
+    [SerializeField] private HammerRobotMovement m_hammerRobotMovement = null;
+    [SerializeField] private ServiceRobotMovement m_serviceRobotMovement = null;
+
+    public static CharController Instance
+    {
+        get; private set;
+    }
+
+    public void SetCharState(CharStates _newCharState)
+    {
+        m_currentCharState = _newCharState;
+        m_playerInput.SwitchCurrentActionMap(GetCurrentPlayerInputMap());
+        m_currentMovement = GetCurrentStateMovement();
+    }
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private string GetCurrentPlayerInputMap()
+    {
+        switch (m_currentCharState)
+        {
+            case CharStates.DEFAULT_HUMAN:
+                return m_defaultHumanInputName;
+
+            case CharStates.BOX_ROBOT:
+                return m_boxRobotInputName;
+
+            case CharStates.HAMMER_ROBOT:
+                return m_hammerRobotInputName;
+
+            case CharStates.SERVICE_ROBOT:
+                return m_serviceRobotInputName;
+
+            default:
+                return m_defaultHumanInputName;
+        }
+    }
+
+    private CharMovement GetCurrentStateMovement()
+    {
+        switch (m_currentCharState)
+        {
+            case CharStates.DEFAULT_HUMAN:
+                return m_humanMovement;
+
+            case CharStates.BOX_ROBOT:
+                return m_boxRobotMovement;
+
+            case CharStates.HAMMER_ROBOT:
+                return m_hammerRobotMovement;
+
+            case CharStates.SERVICE_ROBOT:
+                return m_serviceRobotMovement;
+
+            default:
+                return m_humanMovement;
+        }
+    }
+
+    private void OnEnable()
+    {
+        m_playerInput.SwitchCurrentActionMap(GetCurrentPlayerInputMap());
+    }
+
+    private void OnDisable()
+    {
+        m_playerInput.DeactivateInput();
+    }
+
+    private void Start()
+    {
+        m_currentHealth = m_maxHealth;
+        m_currentMovement = m_humanMovement;
+    }
+
+    private void Update()
+    {
+        // TODO movement depending on game state 
+
+        if(m_currentMovement != null)
+        {
+            m_currentMovement.UpdateMovement();
+        }
+    }
+}
