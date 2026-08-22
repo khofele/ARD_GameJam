19d4b34bb6a6644ab8d28b8166db0ac5f47d9ae9
khofele <karen.hofele@gmx.de>
2026-08-22 14:58:42 +0200

    [added] enemy classes

---
+++ ARD_GameJam/Assets/Code/Scripts/Enemy/ServiceRobotEnemy.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ServiceRobotEnemy : Enemy
+{
+    // TODO implement
+
+    protected override void UpdateMovement()
+    {
+        // TODO might not be needed --> if no, remove in parent class
+    }
+}
